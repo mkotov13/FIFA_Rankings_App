8e26f60c549542cff3409c03de2dda264ab4c1b9
mkotov13 <mkotov13@simons-rock.edu>
2016-12-28 04:06:50 -0500

    dashboard data and styling

--- lib/dashboard_component.dart
+++ lib/dashboard_component.dart
@@ -1,9 +1,26 @@
+import 'dart:async';
+
 import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
+
+import 'player.dart';
+import 'player_service.dart';
 
 @Component(
     selector: 'my-dashboard',
-    template: '''<h3>My Dashboard</h3>'''
+    templateUrl: 'dashboard_component.html',
+    styleUrls: const ['dashboard_component.css'],
+  directives: const [ROUTER_DIRECTIVES],
 )
 
-class DashboardComponent {
+class DashboardComponent implements OnInit{
+  List<Player> players;
+
+  final PlayerService _playerService;
+
+  DashboardComponent(this._playerService);
+
+  Future<Null> ngOnInit() async {
+    players = (await _playerService.getPlayers()).skip(1).take(4).toList();
+  }
 }
--- lib/mock_players.dart
+++ lib/mock_players.dart
@@ -4,5 +4,7 @@ final List<Player> mockPlayers = [
   new Player(11, 'Max', 'Russia', 'Chelsea', 'Maxing (obnoxious and unnecessary flexing)'),
   new Player(12, 'Thomas', 'South Korea', 'FC Barcelona', 'Actually, he is not that good, so we are okay with his playing style'),
   new Player(13, 'Zyaid', 'Jordan', 'Real Madrid', 'Zyaiding (excessive and disgusting slide tackling)'),
-  new Player(1, 'Michael', 'USA', 'Bayern Munich', 'Long shots and trying to spin (without success)')
+  new Player(1, 'Michael', 'USA', 'Bayern Munich', 'Long shots and trying to spin (without success)'),
+  new Player(15, 'Zyaid', 'Jordan', 'Real Madrid', 'Zyaiding (excessive and disgusting slide tackling)'),
+  new Player(17, 'Michael', 'USA', 'Bayern Munich', 'Long shots and trying to spin (without success)')
 ];
